eb2b2748ea85c48d1bd172c3c59096990e922560
H2g <3240101951@zju.edu.cn>
2026-08-31 15:00:01 +0800

    Fix notebook data format

--- notes.js
+++ notes.js
@@ -4,7 +4,7 @@ window.MAIDATA_CONFIG = {
   viewerUrl: "./viewer/"
 };
 
-window.MAIDATA_NOTES = {
+window.MAIDATA_NOTES = [{
     "id":  "note-20260831-145401",
     "title":  "Para Para Sakura",
     "description":  "",
@@ -16,4 +16,4 @@ window.MAIDATA_NOTES = {
     "bpm":  151,
     "addedAt":  "2026-08-31",
     "maidata":  "{8}2b-4[8:1],3,1-5[8:1],8,2\u003c7[8:1],3,4-8[8:1],5,\n{8}2-4[8:1],3,1-5[8:1],7,8-6b[8:1],7,1b/8b,,"
-};
+}];
